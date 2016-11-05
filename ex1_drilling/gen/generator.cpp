f752e2c39b978ec0c48685af02b2eb5acfd1b85b
snate <valle.sebastiano93@gmail.com>
2016-11-05 16:53:52 +0100

    1) Fix generator

--- ex1_drilling/gen/generator.cpp
+++ ex1_drilling/gen/generator.cpp
@@ -7,11 +7,25 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 class IllegalArgumentException {
 };
+
+vector<vector<int> > randSymmMatrixGen(int dim) {
+  vector<vector<int> > matrix;
+  matrix.resize(dim);
+  for (int i = 0; i < dim; i++) {
+    matrix[i].resize(dim);
+    matrix[i][i] = 0;
+  }
+  for (int i = 0; i < dim; i++)
+    for (int j = 0; j < i; j++)
+      matrix[i][j] = matrix[j][i] = rand() % 100 + 1;
+  return matrix;
+}
 
 int main(int argc, char* argv[]) {
   if(argc < 2) throw new IllegalArgumentException();
@@ -19,8 +33,13 @@ int main(int argc, char* argv[]) {
   int limit = *argv[1] - '0';
   ofstream file;
   file.open("../data");
-  for(int i=0; i < limit; i++)
-    file<<i<<'\t'<<(rand() % 100 + 1)<<endl;
+  vector<vector<int> > distances = randSymmMatrixGen(limit);
+  for(int i=0; i < limit; i++) {
+    file<<i;
+    for (int j = 0; j < limit; j++)
+      file<<'\t'<<distances[i][j];
+    file<<endl;
+  }
   file.close();
   return 0;
 }
