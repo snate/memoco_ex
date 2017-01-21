7addb4793d6c2954962ae0c9ea983ea0ffab2e7a
snate <sebastiano.valle@studenti.unipd.it>
2017-01-21 13:41:54 +0100

    Fix time profiler script

--- ex1_drilling/scripts/profiling/take_times.sh
+++ ex1_drilling/scripts/profiling/take_times.sh
@@ -1,9 +1,9 @@
 holes=$1
 size=$2
 divisions=$3
-(cd gen; bash ../scripts/generation/generateRandomData_ru.sh $count $size)
-(cd gen; bash ../scripts/generation/generateRandomData_rg.sh $count $size $div)
-(cd gen; bash ../scripts/generation/generateRandomData_rg_Manh.sh $count $size $div)
+(cd gen; bash ../scripts/generation/generateRandomData_ru.sh $holes $size)
+(cd gen; bash ../scripts/generation/generateRandomData_rg.sh $holes $size $divisions)
+(cd gen; bash ../scripts/generation/generateRandomData_rg_Manh.sh $holes $size $divisions)
 for (( i = 0; i < 7; i++ )); do
 	bash scripts/run/run_ru.sh $holes $size
 	bash scripts/run/run_rg.sh $holes $size $divisions
