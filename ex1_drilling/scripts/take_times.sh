9f144ddfcabee6980db5e92d06d4423f4e681f13
snate <sebastiano.valle@studenti.unipd.it>
2017-01-21 13:37:17 +0100

    Add scripts to take 7 time measurings from each instance

---
+++ ex1_drilling/scripts/take_times.sh
@@ -0,0 +1,10 @@
+holes=$1
+size=$2
+divisions=$3
+for (( i = 0; i < 7; i++ )); do
+	bash scripts/run/run_ru.sh $holes $size
+	bash scripts/run/run_rg.sh $holes $size $divisions
+	bash scripts/run/run_345.sh
+	bash scripts/run/run_tsp12.sh
+	bash scripts/run/run_tsp60.sh
+done
