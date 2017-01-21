74c7ac49250e9c2b6ed0de56f9df264072307471
snate <sebastiano.valle@studenti.unipd.it>
2017-01-21 13:38:21 +0100

    Fix data generation

--- ex1_drilling/scripts/profiling/take_times.sh
+++ ex1_drilling/scripts/profiling/take_times.sh
@@ -4,6 +4,7 @@ divisions=$3
 for (( i = 0; i < 7; i++ )); do
 	bash scripts/run/run_ru.sh $holes $size
 	bash scripts/run/run_rg.sh $holes $size $divisions
+	bash scripts/run/run_rg_Manh.sh $holes $size $divisions
 	bash scripts/run/run_345.sh
 	bash scripts/run/run_tsp12.sh
 	bash scripts/run/run_tsp60.sh
